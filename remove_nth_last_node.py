bae4e3225a1a7b53fe4d6f998122844472c7467a
Vasu Patel <patelvasu1997@gmail.com>
2023-11-03 20:38:23 -0500

    Added "Remove Nth Last Node" Data Structure

---
+++ remove_nth_last_node.py
@@ -0,0 +1,23 @@
+
+
+# Remove nth node from the end from a linked list.
+
+def remove_nth_last_node(head, n):
+    
+    right = head
+    left = head
+    
+    for i in range(n):
+        right = right.next
+
+    if not right:
+        return head.next
+    
+    while right.next:
+        right = right.next
+        left = left.next
+
+    left.next = left.next.next
+
+    return head
+
